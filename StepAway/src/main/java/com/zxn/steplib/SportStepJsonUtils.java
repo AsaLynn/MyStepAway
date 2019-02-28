faac441868097de8a5454792df5c07ca9ddabfa7
zxn <zhangxiaoning@avicare.cn>
2019-02-28 13:06:02 +0800

    对外暴漏计算公式

--- StepAway/src/main/java/com/zxn/steplib/SportStepJsonUtils.java
+++ StepAway/src/main/java/com/zxn/steplib/SportStepJsonUtils.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 /**
@@ -40,13 +41,22 @@ public class SportStepJsonUtils {
     }
 
     // 公里计算公式
-    static String getDistanceByStep(long steps) {
+    public static String getDistanceByStep(long steps) {
         return String.format("%.2f", steps * 0.6f / 1000);
     }
 
     // 千卡路里计算公式
-    static String getCalorieByStep(long steps) {
+    public static String getCalorieByStep(long steps) {
         return String.format("%.1f", steps * 0.6f * 60 * 1.036f / 1000);
     }
+
+    //时间计算公式(粗略)
+    public static String getTimeByStep(long steps) {
+        float v = steps * (1f / 60f / 2f);
+        DecimalFormat df = new DecimalFormat("0.00");
+        //return String.format("%.1f", steps * 0.6f * 60 * 1.036f / 1000);
+        return String.valueOf(df.format(v));
+    }
+
 
 }
